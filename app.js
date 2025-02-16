8b7b7b571b6d74ca8dcd087e1aa8978795432492
HildaAb <ilona.aase@gmail.com>
2025-02-16 18:53:12 +0100

    added functions and style

---
+++ app.js
@@ -0,0 +1,65 @@
+const apiKey = "7XFhSapeXLduKEzebzRhoFCp0Uk0X41uUNwFUYA48L4";
+
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("#search-input");
+const searchResults = document.querySelector("#search-results");
+const showMoreEl = document.querySelector("#show-btn");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apiKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+  console.log(results);
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("#search-result");
+
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+    imageLink.style.display = "flex";
+    imageLink.style.textDecoration = "none";
+    imageLink.style.maxWidth = "55%";
+    imageLink.style.color = "black";
+    imageLink.style.textTransform = "uppercase";
+    image.style.maxHeight = "400px";
+    image.style.maxWidth = "400px";
+    image.style.margin = "5px";
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if (page > 1) {
+    showMoreEl.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMoreEl.addEventListener("click", () => {
+  searchImages();
+});
